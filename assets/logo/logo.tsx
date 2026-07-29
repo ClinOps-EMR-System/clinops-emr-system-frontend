824198938d773ac0a4142fad8ebc01ce3a7c2027
BenjaminVamp2o5 <bit-023-22@must.ac.mw>
2026-07-29 14:11:29 +0200

    remove props in Logo

---
+++ assets/logo/logo.tsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div className="flex flex-col leading-none">
+      <span className="text-lg font-bold text-gray-900 dark:text-white">
+        ClinOps
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
